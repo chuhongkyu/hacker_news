feea8f170c207a0e23f95ae55a44127850153f6b
Mr.chu <chuhongkyu@gmail.com>
2022-06-23 21:46:21 +0900

    한줄로 쓰기

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import Banner from "./Banner";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
 
 function App() {
-  return null;
+  return (
+    <Wrapper>
+      <Banner
+        text={
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quia aliquam, eos ratione aperiam eius eum laborum enim, ullam iste aut. Excepturi sed exercitationem culpa ab, modi deserunt cum. Nisi."
+        }
+      />
+    </Wrapper>
+  );
 }
 
 export default App;
