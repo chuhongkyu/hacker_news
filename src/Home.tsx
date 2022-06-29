d78aacc83cd760febe5fa4ada6d76b851b5eab75
Mr.chu <chuhongkyu@gmail.com>
2022-06-29 14:36:00 +0900

    location #

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import styled from "styled-components";
+
 export interface INews {
   comments_count: number;
   domain: string;
@@ -11,17 +14,46 @@ export interface INews {
   user: string;
 }
 
+const Item = styled.div`
+  padding: 5px 10px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 10px;
+  &:hover {
+    transform: translateY(-5px);
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
 const Home = () => {
   const ajax = new XMLHttpRequest();
-  ajax.open("GET", "https://api.hnpwa.com/v0/news/1.json", false);
-  //메소드 방법, url, boolean false 동기적으로 가져오겠다
+  const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+  const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+  ajax.open("GET", NEWS_URL, false);
   ajax.send();
+  const onHashChange = () => {
+    window.addEventListener("hashchange", () => {
+      ajax.open("GET", CONTENT_URL, false);
+      console.log(window.location.hash);
+    });
+  };
+
   const newsFeed = JSON.parse(ajax.response);
   console.log(newsFeed);
   return (
     <>
       {newsFeed.map((m: INews) => (
-        <h1>{m.title}</h1>
+        <Item key={m.id} onClick={onHashChange}>
+          <a href={`#${m.id}`}>
+            <h1>{m.title}</h1>
+            <p>댓글 : {m.comments_count}</p>
+          </a>
+        </Item>
       ))}
     </>
   );
